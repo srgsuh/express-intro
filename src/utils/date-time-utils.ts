0b3468677417d71599d1760c9659b8e91c915254
srgsuh <srg.suhoverhov@gmail.com>
2025-08-02 11:58:45 +0300

    - Add config loading - Add format loading from the config

--- src/utils/date-time-utils.ts
+++ src/utils/date-time-utils.ts
@@ -1,3 +1,7 @@
 import { format } from "date-fns";
+import {getConfigValue} from "./config-utils.js";
 
-export const formatDate = (date: Date) => format(date, "yyyy-MM-dd HH:mm:ss zzzz");
+const DEFAULT_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss zzz";
+const dateFormat = getConfigValue<string>("format.date", DEFAULT_DATE_FORMAT);
+console.log(dateFormat);
+export const formatDate = (date: Date) => format(date, dateFormat);
