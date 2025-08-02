1cb430318821d98da5b9099cd59a96ad5a324b50
srgsuh <srg.suhoverhov@gmail.com>
2025-08-02 17:26:06 +0300

    Minor imports fixes

--- src/utils/date-time-utils.ts
+++ src/utils/date-time-utils.ts
@@ -1,5 +1,5 @@
 import { format } from "date-fns";
-import {getConfigValue} from "./config-utils.js";
+import {getConfigValue} from "./config-utils.ts";
 
 const DEFAULT_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss zzz";
 const dateFormat = getConfigValue<string>("format.date", DEFAULT_DATE_FORMAT);
